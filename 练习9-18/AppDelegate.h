7ec6c97c100ff4d10a1aa30065cbd25a4dbcdd1f
dc005 <dc005@jixiaogang.lan>
2016-09-18 09:31:35 +0800

    Initial Commit

---
+++ 练习9-18/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  练习9-18
+//
+//  Created by dc005 on 16/9/18.
+//  Copyright © 2016年 dc005. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 练习9-18/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  练习9-18
+//
+//  Created by dc005 on 16/9/18.
+//  Copyright © 2016年 dc005. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
